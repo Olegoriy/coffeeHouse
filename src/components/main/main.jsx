c093b7c362b0051b2022baae3d0801f3a275483a
Olegoriy <olegkib1506@gmail.com>
2025-03-16 22:04:08 +0300

    add: ready layout for main page

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -8,6 +8,9 @@ const Main = () => {
             <Navigation />
             <h1 className="slogan">Everything You Love About Coffee</h1>
             <img className="white__beans" src={require('./img/beans__white.svg').default} alt="Coffee house logo" />
+            <p>We makes every day full of energy and taste</p>
+            <p>Want to try our beans?</p>
+            <button>More</button>
         </div>
     )
 }
